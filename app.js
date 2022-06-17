5ee825966cc75e37ee543b577558c06e0cf303a8
unknown <framin85@gmail.com>
2022-06-17 11:23:59 +0300

    work

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 const TelegramBot = require('node-telegram-bot-api');
 var moment = require('moment-timezone');
-var schedule = require('node-schedule-tz');
+const nodeCron = require("node-cron");
 require('dotenv').config();
 
 const token = process.env.token;
 const bot = new TelegramBot(token, { polling: true });
+let chatId = [];
 
-
-function startApp() {
+async function startApp() {
+    let restTime = await moment.tz('America/Chicago').format('hh:mm A');
+    let weekend = await moment.tz('America/Chicago').day();
+    console.log("start bot " + restTime);
     const nowHours = new Date().getHours()
-    const restTime = moment.tz('America/Chicago').format('H:MMA');
-    const weekend = moment.tz('America/Chicago').day()
-    console.log(`startApp StartJob time: ${restTime}`)
     const restMessage = {
-        status: 0,
         text: "As our hours of operation 8AM - 5PM central time we are no longer by computer, please leave your message and we will assist you tomorrow. "
     }
     const weekendMessage = {
-        status: 0,
         text: "Hello! Our hours of operation are Monday through Friday 8AM - 5PM central time. We address accidents questions over the weekend only."
     }
 
     function startBot() {
-        return bot.on('message', (msg) => {
-            console.log(msg)
-            if (msg.text === '/check') {
-                bot.sendMessage(msg.from.id, `Time in Chicago now ${restTime} \n Day of week ${weekend} \n Status restMessage ${restMessage.status} \n Status Weekend ${weekendMessage.status}`)
+        return bot.on('message', async(msg) => {
+            if (msg.text === '/start') {
+                return false
             }
-            if ((weekend === 6 || weekend === 0) && weekendMessage.status == 0) {
-                weekendMessage.status = restMessage.status = 1;
+            let restTime = await moment.tz('America/Chicago').format('hh:mmA');
+            let weekend = moment.tz('America/Chicago').day();
+            if (msg.text === '/check') {
+                bot.sendMessage(msg.from.id, `Time in Chicago now ${restTime} \n Day of week ${weekend} \n Id groups where bot sends message ${chatId} \n`)
+            }
+            if ((weekend === 6 || weekend === 0) && !chatId.includes(msg.chat.id)) {
+                await chatId.push(msg.chat.id);
                 return bot.sendMessage(msg.chat.id, weekendMessage.text)
-            } else if (((restTime.includes("AM") && Number(restTime[0]) < 8) || (restTime.includes("PM" && Number(restTime[0]) > 17))) && restMessage.status === 0) {
-                console.log(restTime)
-                restMessage.status = true;
+            } else if (((restTime.includes("AM") && Number(restTime[0] + restTime[1]) < 8) || (restTime.includes("PM") && Number(restTime[0] + restTime[1]) >= 5)) && !chatId.includes(msg.chat.id)) {
+                await chatId.push(msg.chat.id);
                 return bot.sendMessage(msg.chat.id, restMessage.text)
 
             } else {
@@ -42,4 +43,5 @@ function startApp() {
     }
     startBot()
 }
-let restart = schedule.scheduleJob('0 17 * * 0-5', 'America/Chicago', startApp());
+startApp()
+job = nodeCron.schedule('0 17 * * 1-5', () => { chatId = [] }, { timezone: 'America/Chicago' });
