a322f76c9a05794684896f9c57c6d3ac6e435f41
unknown <framin85@gmail.com>
2022-06-22 18:01:56 +0300

    finally admin

--- admin.js
+++ admin.js
@@ -2,18 +2,99 @@ require('dotenv').config();
 const fs = require('fs')
 const pass = new RegExp(process.env.passwordAdmin)
 let data = {
-    weekend:"somthing",
-    restTime:"somthingElse"
+    weekend: "somthing",
+    restTime: "somthingElse"
 }
-function admin(bot) {
-    bot.onText(pass,(msg)=>{
-        bot.sendMessage(msg.from.id, "Good you are admin.\n You have menu for change message")
-    });
-    bot.onText(/\/msgWorkDay/,(msg)=>{
-        bot.sendMessage(msg.from.id, "Enter th message work day end prees SEND").then(data=>{
-            if (data.message_id+1) console.log (data)
-            })
-        })
+const keyboard = {
+    reply_markup: {
+        inline_keyboard: [
+            [{
+                text: "Change work day message text ",
+                callback_data: "ChangeRDM"
+            }],
+            [{
+                text: "Change weekend message text",
+                callback_data: "ChangeWDM"
+            }],
+            [{
+                text: "Exit admin mode",
+                callback_data: "exit"
+            }]
+        ]
     }
+}
+let count = 0;
 
-module.exports = {admin}
+function admin(bot, mode) {
+    while (count > 0) {
+        bot.removeTextListener(pass)
+        count--
+    }
+    bot.onText(pass, (msg) => {
+        bot.sendMessage(msg.from.id, "Good you are admin.\n You have menu for change message\n ", keyboard).then((data) => listenBtn(bot, mode, data.message_id))
+
+        bot.removeTextListener(pass)
+    })
+    count++;
+}
+
+function listenBtn(bot, mode, id) {
+    mode = true;
+    bot.on("callback_query", async(msg) => {
+        switch (msg.data) {
+            case ("ChangeRDM"):
+                bot.sendMessage(msg.from.id, "Enter th message work day end prees SEND")
+                bot.onText(/[\W|\w|\d]/, async(msg) => {
+                        let text = await msg.text;
+                        bot.removeTextListener(/[\W|\w|\d]/);
+                        return new Promise((res) => { fileData(text, false) })
+                            .then(bot.sendMessage(msg.from.id, "Thank you. Your message is save"))
+                    })
+                    // bot.sendMessage(msg.from.id, "Thank you. Your message is save")
+                break;
+            case ("ChangeWDM"):
+                bot.sendMessage(msg.from.id, "Enter th message weekend end prees SEND")
+                bot.onText(/[\W|\w|\d]/, async(msg) => {
+                    let text = await msg.text;
+                    bot.removeTextListener(/[\W|\w|\d]/);
+                    return new Promise((res) => { fileData(false, text) })
+                        .then(bot.sendMessage(msg.from.id, "Thank you. Your message is save"))
+                })
+                break;
+            case ("exit"):
+                bot.sendMessage(msg.from.id, "Thank you. Admin mode is off")
+                mode = false;
+                let chatId = await msg.message.chat.id;
+                let msgId = await id;
+                bot.deleteMessage(chatId, msgId)
+
+        }
+    })
+}
+const fileData = async(rest, weekend) => {
+    const fileJson = fs.readFileSync('data.json')
+    const dataFile = JSON.parse(fileJson)
+    if (weekend === false && rest.length > 0) {
+        dataFile.restTime = await rest;
+        try {
+            fs.writeFileSync('data.json', JSON.stringify(dataFile))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    if (rest === false && weekend.length > 0) {
+        dataFile.weekend = await weekend;
+        try {
+            fs.writeFileSync('data.json', JSON.stringify(dataFile))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    return true
+}
+
+
+module.exports = {
+    admin,
+    listenBtn
+}
--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 const TelegramBot = require('node-telegram-bot-api');
 var moment = require('moment-timezone');
 const nodeCron = require("node-cron");
-const {
-    admin
-} = require('./admin');
+const fs = require('fs')
+const { admin, listenBtn } = require('./admin');
 require('dotenv').config();
 const token = process.env.token;
 const bot = new TelegramBot(token, {
     polling: true
 });
 let chatId = [];
-
+let dataMessage = (() => {
+    try {
+        let js = fs.readFileSync('data.json');
+        return JSON.parse(js);
+    } catch (err) {
+        concole.log(err)
+    }
+})
 async function startApp() {
+    listenBtn(bot)
     let restTime = await moment.tz('America/Chicago').format('hh:mm A');
     let weekend = await moment.tz('America/Chicago').day();
     console.log("start bot " + restTime);
     const nowHours = new Date().getHours()
-    const restMessage = {
-        text: process.env.restText
-    }
-    const weekendMessage = {
-        text: process.env.weekendText
-    }
+        // const restMessage = {
+        //     text: process.env.restText
+        // }
+        // const weekendMessage = {
+        //     text: process.env.weekendText
+        // }
     let adminFlag = false;
     bot.setMyCommands([{
         command: 'admin',
@@ -30,23 +37,20 @@ async function startApp() {
 
     function startBot() {
         try {
-            bot.on('message', async (msg) => {
+            bot.on('message', async(msg) => {
                 let restTime = await moment.tz('America/Chicago').format('hh:mmA');
                 let weekend = moment.tz('America/Chicago').day();
                 switch (msg.text) {
                     case '/admin':
-                        bot.sendMessage(msg.from.id, 'Enter admin password').then(data => {
-                            if (msg.txt = process.env.passwordAdmin) {
-                                adminFlag = true;
-                                admin(bot)
-                            }
-                        })
+                        bot.sendMessage(msg.from.id, 'Enter admin password');
+                        console.log(admin(bot, adminFlag))
                         break;
                     case '/start':
                         return false
                         break;
                     case '/check':
-                        await bot.sendMessage(msg.from.id, `Time in Chicago now ${restTime} \n Day of week ${weekend} \n Id groups where bot sends message ${chatId} \n`)
+                        await bot.sendMessage(msg.from.id, `Time in Chicago now ${restTime} \n Day of week ${weekend} \n Id groups where bot sends message ${chatId}
+                         \n Work day Message ${dataMessage().restTime} \n Weekend message ${dataMessage().weekend}`)
                         break;
                     default:
                         if (adminFlag == false) {
