1f9480307262d25c659c26c740ea080ac9aa2e46
unknown <framin85@gmail.com>
2022-06-27 20:14:43 +0300

    troble with adminFlag

--- admin.js
+++ admin.js
@@ -2,58 +2,85 @@ require('dotenv').config();
 const fs = require('fs')
 const pass = process.env.passwordAdmin
 const checkPass = new RegExp(pass)
-const wrongPass = new RegExp (`^(?!.*${pass}*).+$`);
-console.log(wrongPass)
+const wrongPass = new RegExp(`^(?!.*${pass}*).+$`);
+let adminFlag = false;
 let data = {
     weekend: "somthing",
     restTime: "somthingElse"
 }
+async function admincheck(bot, msg, mode) {
+    console.log(mode)
+    if (mode === true) {
+        bot.sendMessage(msg.from.id, "You already status admin")
+        return true;
+    }
+    if (mode == false) {
+        let check = await bot.sendMessage(msg.from.id, 'Enter admin password');
+        return admin(bot)
+    }
+}
 
 function admin(bot, mode) {
-        bot.onText(checkPass, (msg) => {
-            bot.sendMessage(msg.from.id, "Good you are admin.\n You have menu for change message\n ", require('./menu')).then((data) => {listenBtn(bot, mode, data.message_id)})
-            bot.removeTextListener(checkPass)
-            bot.removeTextListener(wrongPass)
-        })
-        bot.onText(wrongPass,(msg)=>{
-            bot.sendMessage(msg.from.id, 'Password is wrong')
-            bot.removeTextListener(wrongPass)
-            bot.removeTextListener(checkPass)
-        })
+    bot.onText(checkPass, async(msg) => {
+        bot.sendMessage(msg.from.id, "Good you are admin.\n You have menu for change message\n ", require('./menu'))
+            .then((data) => {
+                return listenBtn(bot, mode, data.message_id)
+            })
+        bot.removeTextListener(checkPass)
+        bot.removeTextListener(wrongPass)
+    })
+
+    bot.onText(wrongPass, (msg) => {
+        bot.sendMessage(msg.from.id, 'Password is wrong')
+        bot.removeTextListener(wrongPass)
+        bot.removeTextListener(checkPass)
+    })
+    return true
 }
 
-function listenBtn(bot, mode, id) {
-        console.log('start listener');
+let listenBtn = (bot, mode, id) => {
+    console.log('start listener');
     bot.on("callback_query", (msg) => {
-        adminMenu(bot,msg,id);
-    })  
+        adminMenu(bot, msg, id);
+    })
 }
-const adminMenu = async (bot, callback, id)=>{
+const adminMenu = async(bot, callback, id) => {
+
     switch (callback.data) {
         case ("ChangeRDM"):
             bot.sendMessage(callback.from.id, "Enter th message work day end prees SEND")
             bot.onText(/[\W|\w|\d]/, async(msg) => {
-                    let text = await msg.text;
-                    bot.removeTextListener(/[\W|\w|\d]/);
+                let text = await msg.text;
+                bot.removeTextListener(/[\W|\w|\d]/);
+                if (text.length > 0) {
                     return new Promise((res) => { fileData(text, false) })
                         .then(bot.sendMessage(msg.from.id, "Thank you. Your message is save"))
-                })
+                } else {
+                    bot.sendMessage(msg.from.id, 'Wrong text data')
+                }
+            })
             break;
         case ("ChangeWDM"):
             bot.sendMessage(callback.from.id, "Enter th message weekend end prees SEND")
             bot.onText(/[\W|\w|\d]/, async(msg) => {
                 let text = await msg.text;
                 bot.removeTextListener(/[\W|\w|\d]/);
-                return new Promise((res) => { fileData(false, text) })
-                    .then(bot.sendMessage(msg.from.id, "Thank you. Your message is save"))
+                if (text.length > 0) {
+                    let save = new Promise((res) => { fileData(false, text) })
+                        .then(bot.sendMessage(msg.from.id, "Thank you. Your message is save"))
+                } else {
+                    return bot.sendMessage(msg.from.id, 'Wrong text data')
+                }
             })
             break;
         case ("exit"):
             bot.sendMessage(callback.from.id, "Thank you. Admin mode is off")
             let chatId = await callback.message.chat.id;
             let msgId = await id;
+            console.log(msgId)
             bot.deleteMessage(chatId, msgId)
-            exit();
+            bot.removeListener("callback_query");
+            adminFlag = false;
     }
 }
 
@@ -78,10 +105,9 @@ const fileData = async(rest, weekend) => {
     }
     return false
 }
-function exit (){
-    return false;
-}
+
 
 module.exports = {
-    admin, exit
+    admincheck,
+    adminFlag
 }
--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const TelegramBot = require('node-telegram-bot-api');
 var moment = require('moment-timezone');
 const nodeCron = require("node-cron");
 const fs = require('fs')
-const {
-    admin, exit
+let {
+    admincheck,
+    adminFlag
 } = require('./admin');
 require('dotenv').config();
 const token = process.env.token;
@@ -19,32 +20,26 @@ let dataMessage = (() => {
         concole.log(err)
     }
 })
+console.log(adminFlag)
 async function startApp() {
     let restTime = await moment.tz('America/Chicago').format('hh:mm A');
     let weekend = await moment.tz('America/Chicago').day();
     console.log("start bot " + restTime);
     const nowHours = new Date().getHours()
-    let adminFlag = exit();
     bot.setMyCommands([{
         command: 'admin',
         description: "admin panel to control bot"
     }])
 
+
     function startBot() {
         try {
-            bot.on('message', async (msg) => {
+            bot.on('message', async(msg) => {
                 let restTime = await moment.tz('America/Chicago').format('hh:mmA');
-                let weekend = moment.tz('America/Chicago').day();
+                let weekend = await moment.tz('America/Chicago').day();
                 switch (msg.text) {
                     case '/admin':
-                        if (adminFlag === true){
-                            bot.sendMessage (msg.from.id, "You already status admin")
-                        }
-                        if (adminFlag== false){
-                            let check =await bot.sendMessage(msg.from.id, 'Enter admin password');
-                            admin(bot, adminFlag)
-                         }
-
+                        adminFlag = await admincheck(bot, msg, adminFlag)
                         break;
                     case '/start':
                         return false
@@ -54,18 +49,16 @@ async function startApp() {
                          \n Work day Message ${dataMessage().restTime} \n Weekend message ${dataMessage().weekend}`)
                         break;
                     default:
-                        if (adminFlag == false) {
-                            if (weekend === 5 && (restTime.includes("PM") && Number(restTime[0] + restTime[1]) >= 5) && !chatId.includes(msg.chat.id)) {
-                                await chatId.push(msg.chat.id);
-                                return bot.sendMessage(msg.chat.id, weekendMessage.text);
-                            }
-                            if ((weekend === 6 || weekend === 0) && !chatId.includes(msg.chat.id)) {
-                                await chatId.push(msg.chat.id);
-                                return bot.sendMessage(msg.chat.id, dataMessage().weekend)
-                            } else if (((restTime.includes("AM") && Number(restTime[0] + restTime[1]) < 8) || (restTime.includes("PM") && Number(restTime[0] + restTime[1]) >= 5)) && !chatId.includes(msg.chat.id)) {
-                                await chatId.push(msg.chat.id);
-                                return bot.sendMessage(msg.chat.id, dataMessage().restTime)
-                            }
+                        if (weekend === 5 && (restTime.includes("PM") && Number(restTime[0] + restTime[1]) >= 5) && !chatId.includes(msg.chat.id)) {
+                            await chatId.push(msg.chat.id);
+                            return bot.sendMessage(msg.chat.id, weekendMessage.text);
+                        }
+                        if ((weekend === 6 || weekend === 0) && !chatId.includes(msg.chat.id)) {
+                            await chatId.push(msg.chat.id);
+                            return bot.sendMessage(msg.chat.id, dataMessage().weekend)
+                        } else if (((restTime.includes("AM") && Number(restTime[0] + restTime[1]) < 8) || (restTime.includes("PM") && Number(restTime[0] + restTime[1]) >= 5)) && !chatId.includes(msg.chat.id)) {
+                            await chatId.push(msg.chat.id);
+                            return bot.sendMessage(msg.chat.id, dataMessage().restTime)
                         }
                 }
             });
