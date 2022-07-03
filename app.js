9eb4bb920c1aa79eab8a758d9113a156e26f8116
Perokar <framin85@gmail.com>
2022-07-03 22:36:45 +0300

    win 12time

--- app.js
+++ app.js
@@ -1,81 +1,60 @@
 const TelegramBot = require('node-telegram-bot-api');
 var moment = require('moment-timezone');
 const nodeCron = require("node-cron");
-const fs = require('fs')
-let {
-    admincheck,
-    adminFlag
-} = require('./admin');
+const fs = require('fs');
 require('dotenv').config();
 const token = process.env.token;
 const bot = new TelegramBot(token, {
     polling: true
 });
 let chatId = [];
-let dataMessage = (() => {
-    try {
-        let js = fs.readFileSync('data.json');
-        return JSON.parse(js);
-    } catch (err) {
-        concole.log(err)
-    }
-})
-console.log(adminFlag)
+let dataMessage = () => {
+    let fileJson = fs.readFileSync('data.json')
+    return JSON.parse(fileJson);
+}
+
 async function startApp() {
     let restTime = await moment.tz('America/Chicago').format('hh:mm A');
     let weekend = await moment.tz('America/Chicago').day();
     console.log("start bot " + restTime);
-    const nowHours = new Date().getHours()
-    bot.setMyCommands([{
-        command: 'admin',
-        description: "admin panel to control bot"
-    }])
-
 
     function startBot() {
-        try {
-            bot.on('message', async(msg) => {
-                let restTime = await moment.tz('America/Chicago').format('hh:mmA');
+            bot.on('message', async (msg) => {
+                let restTime = await moment.tz('America/Chicago').format('hh:mm A');
                 let weekend = await moment.tz('America/Chicago').day();
+                let restT = +(restTime[0]+restTime[1]);
                 switch (msg.text) {
-                    case '/admin':
-                        adminFlag = await admincheck(bot, msg, adminFlag)
-                        break;
                     case '/start':
-                        return false
                         break;
                     case '/check':
-                        await bot.sendMessage(msg.from.id, `Time in Chicago now ${restTime} \n Day of week ${weekend} \n Id groups where bot sends message ${chatId}
-                         \n Work day Message ${dataMessage().restTime} \n Weekend message ${dataMessage().weekend}`)
+                        await bot.sendMessage(msg.from.id, `Time in Chicago now ${restTime} \n Day of week ${weekend} \n Id groups where bot sends message ${chatId} \n Text message ====${dataMessage().weekend}`)
                         break;
                     default:
-                        if (weekend === 5 && (restTime.includes("PM") && Number(restTime[0] + restTime[1]) >= 5) && !chatId.includes(msg.chat.id)) {
-                            await chatId.push(msg.chat.id);
-                            return bot.sendMessage(msg.chat.id, weekendMessage.text);
+                        if (weekend === 5 && (restTime.includes("PM") && restT>= 5) && !chatId.includes(await msg.chat.id)) {
+                            chatId.push(await msg.chat.id);
+                            bot.sendMessage(msg.chat.id, dataMessage().weekend);
+                        } 
+                        if ((weekend === 6 || weekend === 0) && !chatId.includes(await msg.chat.id)) {
+                            chatId.push(await msg.chat.id);
+                            bot.sendMessage(msg.chat.id, dataMessage().weekend)
                         }
-                        if ((weekend === 6 || weekend === 0) && !chatId.includes(msg.chat.id)) {
-                            await chatId.push(msg.chat.id);
-                            return bot.sendMessage(msg.chat.id, dataMessage().weekend)
-                        } else if (((restTime.includes("AM") && Number(restTime[0] + restTime[1]) < 8) || (restTime.includes("PM") && Number(restTime[0] + restTime[1]) >= 5)) && !chatId.includes(msg.chat.id)) {
-                            await chatId.push(msg.chat.id);
-                            return bot.sendMessage(msg.chat.id, dataMessage().restTime)
+                       if (((restTime.includes("AM") && ((restT < 8)||(restT===12))) || (restTime.includes("PM") && (restT >= 5)&& (restT!=12))) && !chatId.includes(await msg.chat.id)) {
+                            chatId.push(await msg.chat.id);
+                            bot.sendMessage(msg.chat.id, dataMessage().weekend)
                         }
                 }
             });
-        } catch (err) {
-            console.log('error app ' + err)
-        }
     }
     startBot()
 }
 startApp()
 job = nodeCron.schedule('0 17 * * 1-5', () => {
-    chatId = []
+    chatId = [];
 }, {
     timezone: 'America/Chicago'
 });
 jobWeekend = nodeCron.schedule('0 0 * * 6,0', () => {
-    chatId = []
+    chatId = [];
 }, {
     timezone: 'America/Chicago'
 });
